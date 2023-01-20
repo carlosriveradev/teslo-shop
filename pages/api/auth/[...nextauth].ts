903cbe269e993b012b576a397283882cdff5ed0f
Carlos Rivera <carlosrivera.dev@gmail.com>
2023-01-19 22:54:25 -0500

    try 9

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,68 +1,78 @@
-import NextAuth from 'next-auth'
-import GithubProvider from 'next-auth/providers/github'
-import Credentials from 'next-auth/providers/credentials'
+import NextAuth from 'next-auth';
+import CredentialsProvider from 'next-auth/providers/credentials';
+import GithubProvider from 'next-auth/providers/github';
 import { dbUsers } from '../../../database';
 
+declare module "next-auth" {
+  interface Session {
+    accessToken?: string;
+  }
+}
+
 export default NextAuth({
-  // Configure one or more authentication providers
   providers: [
-    Credentials({
-      name: 'Custom Login',
+    CredentialsProvider({
+      name: 'Teslo Account',
+      type: 'credentials',
       credentials: {
-        email: {label: 'Correo', type: 'email', placeholder: 'correo@google.com'},
-        password: {label: 'Contraseña', type: 'password', placeholder: 'Contraseña'},
+        email: { label: 'Email:', type: 'email', placeholder: 'Email' },
+        password: { label: 'Password:', type: 'password', placeholder: 'Password' },
       },
       async authorize(credentials) {
-        // console.log({credentials});
-        return await dbUsers.checkUserEmailPassword(credentials!.email, credentials!.password)
+        return await dbUsers.checkUserEmailPassword(credentials!.email, credentials!.password);
       }
     }),
-
     GithubProvider({
-      clientId: process.env.GITHUB_ID,
-      clientSecret: process.env.GITHUB_SECRET,
+      clientId: process.env.GITHUB_ID || '',
+      clientSecret: process.env.GITHUB_SECRET || '',
     }),
-    // ...add more providers here
   ],
-  // custom pages
+
+  // Custom Pages
   pages: {
     signIn: '/auth/login',
     newUser: '/auth/register'
   },
-  // callbacks
-  jwt: {
 
-  },
+  // Session
   session: {
-    maxAge: 2592000,
+    maxAge: 2592000, // 30 days
     strategy: 'jwt',
-    updateAge: 86400
+    updateAge: 86400, // Diario
   },
+
+  // Callbacks
   callbacks: {
-    async jwt({token, account, user}) {
-      // console.log({token, account, user});
-      if (account) {
-        token.accessToken = account.access_token
+
+    async jwt({ token, account, user }) {
+      // console.log({ token, account, user })
+
+      if(account) {
+        token.accessToken = account.access_token;
+
         switch (account.type) {
-          case 'oauth':
-            token.user = await dbUsers.oAUthToDbUser(user?.email!, user?.name!)
-          break;
-        
           case 'credentials':
-            token.user=user;
+             token.user = user;
+          break;
+
+          case 'oauth':
+            token.user = await dbUsers.oAUthToDbUser(user?.email || '', user?.name || '')
           break;
         }
       }
-      return token
-    },
-    async session({ session, token, user }){
-      // console.log({ session, token, user });
 
-      session.accessToken = token.accessToken;
+      return token;
+    },
+
+    async session({ session, token, user }) {
+      // console.log({ session, token, user })
+
+      session.accessToken = token.accessToken as any;
       session.user = token.user as any;
 
       return session;
     }
+
   }
 
-})
+});
